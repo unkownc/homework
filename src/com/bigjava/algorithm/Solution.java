5a1d7321da69820adc154ef5228f7ef1d55c030b
ChenJianPing <2674950738@qq.com>
2020-07-14 11:02:58 +0800

    剑指 Offer 05. 替换空格,1389. 按既定顺序创建目标数组,1512. 好数对的数目提交

--- src/com/bigjava/algorithm/Solution.java
+++ src/com/bigjava/algorithm/Solution.java
@@ -3,16 +3,210 @@ package com.bigjava.algorithm;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
 
 public class Solution {
 
 	public static void main(String[] args) {
 		Solution solution = new Solution();
 
-		int[] nums = { 1, 3, 5, 1, 2, 4 };
-		int a = solution.xorOperation(1, 7);
-		System.out.println(a);
+		System.out.println(solution.minCount(new int[] { 2, 3, 10 }));
+	}
+
+	public int minCount(int[] coins) {
+		int sum = 0;
+
+		for (int i : coins) {
+			sum += (i % 2 == 0) ? i / 2 : (i / 2 + 1);
+		}
+
+		return sum;
+	}
+
+	/**
+	 * 剑指 Offer 05. 替换空格
+	 * 
+	 * @param s
+	 * @return
+	 */
+	public String replaceSpace(String s) {
+		StringBuffer s2 = new StringBuffer();
+		for (int i = 0; i < s.length(); i++) {
+			s2.append(' ' == s.charAt(i) ? "%20" : s.charAt(i));
+		}
+		return s2.toString();
+	}
+
+	/**
+	 * 1389. 按既定顺序创建目标数组
+	 * 
+	 * @param nums
+	 * @param index
+	 * @return
+	 */
+	public int[] createTargetArray(int[] nums, int[] index) {
+
+		List<Integer> numbers = new ArrayList<Integer>(nums.length);
+
+		for (int i = 0; i < nums.length; i++) {
+
+			int index2 = index[i];
+
+			numbers.add(index2, nums[i]);
+
+		}
+
+		int[] nums2 = new int[nums.length];
+
+		for (int i = 0; i < numbers.size(); i++) {
+			nums2[i] = numbers.get(i);
+		}
+
+		return nums2;
+	}
+
+	/**
+	 * 1512. 好数对的数目
+	 * 
+	 * @param nums
+	 * @return
+	 */
+	public int numIdenticalPairs(int[] nums) {
+
+		int sum = 0;
+
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = 0; j < nums.length; j++) {
+				if (nums[i] == nums[j] && i < j)
+					sum++;
+			}
+		}
+
+		return sum;
+	}
+
+	/**
+	 * 1342. 将数字变成 0 的操作次数
+	 * 
+	 * @param num
+	 * @return
+	 */
+	public int numberOfSteps(int num) {
+		int sum = 0;
+
+		while (num != 0) {
+			if (num % 2 == 0) {
+				num /= 2;
+			} else {
+				num -= 1;
+			}
+			sum++;
+		}
+
+		return sum;
+	}
+
+	/**
+	 * 1108. IP 地址无效化
+	 * 
+	 * @param address
+	 * @return
+	 */
+	public String defangIPaddr(String address) {
+		String newAddress = "";
+		for (int i = 0; i < address.length(); i++) {
+			if (".".equals(address.charAt(i) + "")) {
+				newAddress += "[.]";
+			} else {
+				newAddress += address.charAt(i);
+			}
+		}
+		return newAddress;
+	}
+
+	/**
+	 * 1281. 整数的各位积和之差
+	 * 
+	 * @param n
+	 * @return
+	 */
+	public int subtractProductAndSum(int n) {
+
+		int sum = 0;
+		int mut = 1;
+
+		while (n != 0) {
+			sum += n % 10;
+			mut *= n % 10;
+			n = n / 10;
+		}
+
+		return mut - sum;
+	}
+
+	/**
+	 * 771. 宝石与石头
+	 * 
+	 * @param J
+	 * @param S
+	 * @return
+	 */
+	public int numJewelsInStones(String J, String S) {
+		int sum = 0;
+
+		char[] chars = S.toCharArray();
+		char[] chars2 = J.toCharArray();
+		for (char c : chars) {
+			for (char c2 : chars2) {
+				if (c == c2) {
+					sum++;
+					break;
+				}
+			}
+		}
+
+		return sum;
+	}
+
+	/**
+	 * LCP 01. 猜数字
+	 * 
+	 * @param guess
+	 * @param answer
+	 * @return
+	 */
+	public int game(int[] guess, int[] answer) {
+		int sum = 0;
+
+		for (int i = 0; i < guess.length; i++) {
+			if (guess[i] == answer[i]) {
+				sum++;
+			}
+		}
+		return sum;
+	}
+
+	/**
+	 * 剑指 Offer 58 - II. 左旋转字符串
+	 * 
+	 * @param s
+	 * @param n
+	 * @return
+	 */
+	public String reverseLeftWords(String s, int n) {
+		return s.substring(n) + s.substring(0, n);
+	}
+
+	public String reverseLeftWords2(String s, int n) {
+		char[] str = s.toCharArray();
+		String newStr = "";
+		for (int i = 0, j = 0; i < str.length; i++) {
+			if ((n + i) >= str.length) {// 左旋转数字加i大于拆分长度
+				newStr += str[j++];// 尾部开始加前面的
+			} else {
+				newStr += str[n + i];// 先添加后面的字符串
+			}
+		}
+		return newStr;
 	}
 
 	/**
